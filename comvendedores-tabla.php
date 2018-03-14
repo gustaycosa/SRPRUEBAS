aee70cca95ad44bbcdd6e82c875b907f2e7e6d11
Gustavo Hdez <gustavo.hernandez@taycosa.mx>
2018-03-14 10:13:50 -0600

    CAMBIOS COMVENDEDORES
    CAMBIOS COMVENDEDORES

--- comvendedores-tabla.php
+++ comvendedores-tabla.php
@@ -1,20 +1,11 @@
 <?php
 
-require_once('lib/nusoap.php');
-
-ini_set("soap.wsdl_cache_enabled", "0");
-
-$Columnas = array("id_empresa","Id_Sucursal","Id_Vendedor","MovimientoTaller","Caso","Cve_Documento","Cve_Aplico","FechaFactura","FechaVence","FechaPago","Plazo","DiaQuePago","Moneda","Subtotal","Iva","Total","Abono","SaldoDocto","Famila","Folio","Id_Cliente","Cliente","Concepto",
-"Articulo","Cantidad" ,"Descripcion","PrecioVenta" ,"Importe" ,"Imp_REF" ,"Imp_MAQ" ,"Imp_MO" ,"Imp_MOP" ,"Porcentage_Imp" ,"Comi_Imp_REF" ,"Comi_Imp_MAQ" ,"Comi_Imp_Vta_Contado" ,"Comi_Imp_MO" ,"Comi_Imp_MOP" ,"Comi_Imp_TRA");//COLUMNAS GRID
-//$De = date('Y-m-d');
-//$A = date('Y-m-d');
-
 try{ 
     
     if ($_POST){
         
         $Empresa = $_POST["CmbEmpresa"];
-        $Clave =  $_POST["Cmbvendedores"];
+        $Clave =  $_POST["TxtClave"];
         $Moneda =  $_POST["CmbMoneda"];
         $De = $_POST["Fini"];
         $A =  $_POST["Ffin"]; 
@@ -43,46 +34,118 @@ try{
   var_dump($e);
 }
 
-echo "<div class='table-responsive'><table id='grid' class='table table-striped table-bordered table-condensed table-hover' cellspacing='0' width='100%' style='white-space: nowrap;'>
-	 	<thead><tr>"; 
-		
-			for($i=0; $i<count($Columnas); $i++){
-		  		echo "<th>".$Columnas[$i]."</th>";
-			}
+    echo "<div class='table-responsive'>
+        <table id='gridvend' class='table table-striped table-bordered table-condensed table-hover display compact' cellspacing='0' width='100%' ><tfoot><tr><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr></tfoot></table></div>";
 
-	  	echo "</tr></thead>
-	 	<tfoot><tr>";
+	$arreglo = [];
+	for($i=0; $i<count($Datos); $i++){
+		$arreglo[$i]=$Datos[$i];
 
-			for($i=0; $i<count($Columnas); $i++){
-		  		echo "<th>".$Columnas[$i]."</th>";
-			}
-	  	
- echo "</tr></tfoot><tbody>";
-    
- for($i=0; $i<count($Datos); $i++){
-    echo "<tr>";
-    for($j=0; $j<count($Columnas); $j++){
-        echo "<td width='auto'>".$Datos[$i]->$Columnas[$j]."</td>";
-    } 
-    echo "</tr>";
- } 
-    
-  echo "</tbody></table></div>";
+	}
 
 ?>
 
-<script type="text/javascript"> 
-        
-    $('#grid').DataTable( {
+     <script type="text/javascript"> 
+        var datos = 
+        <?php 
+            echo json_encode($arreglo);
+        ?>
+		;
+		<?php
+/*
+			$sGridNomb = '#gridfact';
+			$sWsNomb = 'vtas_netasfact';
+			$aColumnas = array("Fecha","Id_Sucursal","Serie","Folio","Id_cliente","Nombre","Concepto","Total");
+			$aTitulos =  array("Fecha","Id_Sucursal","Serie","Folio","Id_cliente","Nombre","Concepto","Total");
+			echo GrdRptShort($sGridNomb,$sWsNomb,$aColumnas,$aTitulos);
+            */
+		?>
+    $(document).ready(function() {
+    $('#gridvend').DataTable( {
+        data:datos,
+        columns: [
+            { data: 'MovimientoTaller' },
+            { data: 'Caso' },
+            { data: 'Cve_Documento' },
+            { data: 'Cve_Aplico' },
+            { data: 'FechaFactura' },
+            { data: 'FechaVence' },
+            { data: 'FechaPago' },
+            { data: 'Plazo' },
+            { data: 'DiaQuePago' },
+            { data: 'Moneda' },   
+            { data: 'Subtotal' },
+            { data: 'Iva' },
+            { data: 'Total' },
+            { data: 'Abono' },
+            { data: 'SaldoDocto' },  
+            { data: 'Famila' },
+            { data: 'Folio' },  
+            { data: 'Id_Cliente' },  
+            { data: 'Cliente' },
+            { data: 'Concepto' },  
+            { data: 'Articulo' },
+            { data: 'Cantidad' },  
+            { data: 'Descripcion' },
+            { data: 'PrecioVenta' },  
+            { data: 'Importe' },
+            { data: 'Imp_REF' },  
+            { data: 'Imp_MAQ' },
+            { data: 'Imp_MO' },  
+            { data: 'Imp_MOP' },  
+            { data: 'Porcentage_Imp' },
+            { data: 'Comi_Imp_REF' },  
+            { data: 'Comi_Imp_MAQ' },
+            { data: 'Comi_Imp_Vta_Contado' },         
+            { data: 'Comi_Imp_MO' },
+            { data: 'Comi_Imp_MOP' },  
+            { data: 'Comi_Imp_TRA' }
+        ],
+        columnDefs: [
+            { 'title': 'MOVIMIENTO', 'targets': 0},
+            { 'title': 'CASO', 'targets': 1},
+            { 'title': 'CLAVE DOC', 'targets': 2},
+            { 'title': 'CLAVE APLICO', 'targets': 3},
+            { 'title': 'FECHA FACTURA', 'targets': 4},
+            { 'title': 'FECHA VENCE', 'targets': 5},
+            { 'title': 'FECHA PAGO', 'targets': 6},
+            { 'title': 'PLAZO', 'targets': 7},
+            { 'title': 'DIA QUE PAGO', 'targets': 8},
+            { 'title': 'MONEDA', 'targets': 9},
+            { 'title': 'SUBTOTAL', 'targets': 10},
+            { 'title': 'IVA', 'targets': 11},
+            { 'title': 'TOTAL', 'targets': 12},
+            { 'title': 'ABONO', 'targets': 13},
+            { 'title': 'SALFO DOC', 'targets': 14},
+            { 'title': 'FAMILIA', 'targets': 15},
+            { 'title': 'FOLIO', 'targets': 16},
+            { 'title': 'ID CLIENTE', 'targets': 17},
+            { 'title': 'CLIENTE', 'targets': 18},
+            { 'title': 'CONCEPTO', 'targets': 19},
+            { 'title': 'ARTICULO', 'targets': 20},
+            { 'title': 'CANTIDAD', 'targets': 21},
+            { 'title': 'DESCRIPCION', 'targets': 22},
+            { 'title': 'PRECIO VENTA', 'targets': 23},
+            { 'title': 'IMPORTE', 'targets': 24},
+            { 'title': 'IMP REF', 'targets': 25},
+            { 'title': 'IMP MAQ', 'targets': 26},
+            { 'title': 'IMP MO', 'targets': 27},
+            { 'title': 'IMP MOP', 'targets': 28},
+            { 'title': 'PORCENTAJE IMP', 'targets': 29},
+            { 'title': 'COMISION IMP REF', 'targets': 30},
+            { 'title': 'COMISION IMP MAQ', 'targets': 31},
+            { 'title': 'COMISION IMP CONT', 'targets': 32},
+            { 'title': 'COMISION IMP MO', 'targets': 33},
+            { 'title': 'COMISION IMP MOP', 'targets': 34},
+            { 'title': 'COMISION IMP TRA', 'targets': 35}
+        ],
+        'createdRow': function ( row, data, index ) {
+            $(row).attr({ id:data.id_vendedor});
+            $(row).addClass('vendedor');
+        }, 
         fixedHeader: true,
         dom: 'lfBrtip',            
         buttons: [
-            {
-                extend: 'colvis',
-                columns: ':not(:first-child)',
-                collectionLayout: 'fixed two-column',
-                text: 'Ocultar columnas'
-            },
             {
                 extend: 'copy',
                 message: 'PDF created by PDFMake with Buttons for DataTables.',
@@ -181,11 +244,13 @@ echo "<div class='table-responsive'><table id='grid' class='table table-striped 
             "oAria": {
                 "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
                 "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-            },
-        "scrollY": '50vh',
-        "scrollX": true,
-        "paging": false
+            }
         },
+        scrollY:        '50vh',
+        scrollX:        'true',
+        scrollCollapse: true,
+        paging:         false,
+        
         "footerCallback": function ( row, data, start, end, display ) {
             var api = this.api(), data;
             //var api2 = this.api(), data;
@@ -308,9 +373,8 @@ echo "<div class='table-responsive'><table id='grid' class='table table-striped 
             );
             
             $("#TotalComisiones").val('$' + totalFinal.toFixed(2) + ' COMISION TOTAL');
+
         }
-
     } );
-
-
+} );
 </script>
--- comvendedores.php
+++ comvendedores.php
@@ -23,10 +23,6 @@
                   </select>
               </div>
               <div class="form-group">
-                  <label for="inputFechaIni">Vendedor:</label>
-                  <?php echo CmbCualquieras('id_vendedor','nombre','vendedores'); ?>
-              </div>
-              <div class="form-group">
                   <label for="inputFechaIni">Moneda:</label>
                   <select id="CmbMoneda" name="CmbMoneda" class="form-control">
                       <option>PESOS</option>
@@ -42,11 +38,12 @@
                 <input type="date" name="Ffin" id="Ffin" value="<?php echo date("Y-m-d");?>" class="form-control" placeholder="Rango Fecha Final" >
               </div>
               <button type="submit" id="btnEnviar" class="btn btn-primary btn-sm" onMouseOver="">Consultar</button>
-                
-                <input type="text"  name="TotalComisiones" id="TotalComisiones" class="form-control">
-                
+              <input type="hidden" name="TxtClave" id="TxtClave" value="">
+               <input type="text"  name="TotalComisiones" id="TotalComisiones" class="form-control">
             </form>
             <div class="respuesta"></div>
+            <label id="lbltotal" style="font-size: 20px; background: yellow;"></label>
+            <?php echo MdlSearchLG('MdlVenDet','Detalle vendedor');?>
         </div>
     </div>
 </body>
@@ -60,17 +57,20 @@
          $("form").on('submit', function (e) {
 
          e.preventDefault();
+         $('#CargaGif').show();
          $('#btnEnviar').attr('disabled', 'disabled')
          $.ajax({
                type: "POST",
-               url: 'comvendedores-tabla.php',
+               url: 'tabla-comvendedoressum.php',
                data: $("form").serialize(), // Adjuntar los campos del formulario enviado.
                success: function(data)
                {
+                   $('#CargaGif').hide();
                    $('#btnEnviar').removeAttr('disabled');
                    $(".respuesta").html(data); // Mostrar la respuestas del script PHP.
                },
                 error: function(error) {
+                    $('#CargaGif').hide();
                     console.log(error);
                     alert('Algo salio mal :S');
                 }
@@ -84,6 +84,30 @@
             var name = $('#Cmbvendedores option:selected').html();
             $("#title").html("Reporte ventas - CLAVE " + id + " - " + name);
             $("#cabecera").html("Reporte ventas - CLAVE " + id + " - " + name);
+        });
+        
+        $(document).on('dblclick','tr.vendedor',function(){
+            var id = $(this).attr("id");
+            $("#TxtClave").val(id);
+            $('#CargaGif').show();
+            $.ajax({
+                type: "POST",
+                url: 'comvendedores-tabla.php',
+                data: $("form").serialize(), // Adjuntar los campos del formulario enviado.
+                success: function(data) {
+                    //$('#btnEnviar').removeAttr('disabled');
+                    $("#DivMdlVenDet").html(data); // Mostrar la respuestas del script PHP.
+                    $("#DivMdlVenDet").show();
+                    $('#MdlVenDet').modal('show');
+                    $('#gridvend').DataTable().draw();
+                },
+                error: function(error) {
+                    $('#CargaGif').hide();
+                    console.log(error);
+                    alert('Algo salio mal :S');
+                }
+            });
+            return false; // Evitar ejecutar el submit del formulario.
         });
     });
 
